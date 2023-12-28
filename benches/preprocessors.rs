6f9223a169056c9cfa007ca5ef88607802980bdb
Adam Killam <116528597+amkillam@users.noreply.github.com>
2023-12-28 04:42:32 +0000

    Vastly improve benchmark

--- benches/preprocessors.rs
+++ benches/preprocessors.rs
@@ -6,15 +6,17 @@ use file_rw::{
     },
     FileReader, FileWriter,
 };
-use rand;
+use rand::Rng;
 use tempfile::tempdir;
 
 macro_rules! stress_test_preprocessor_fn {
-    ($preprocessor:ident, $preprocessor_type:ident, $file_writer:ident, $data_len:ident, |$byte:ident, $replace_byte:ident, $preprocessor_cache:ident| $block:block) => {
+    ($preprocessor_type:ident, $file_writer:ident, $data_len:ident, |$byte:ident, $replace_byte:ident, $preprocessor_cache:ident, $digit:ident| $block:block) => {
         for $digit in 0..$data_len {
-            let mut $preprocess_cache = $file_writer.preprocess_with::<$preprocessor_type>();
-            for $byte in 0..0xFF {
-                let $replace_byte = 0xFF - $byte;
+            let mut $preprocessor_cache = $file_writer.preprocess_with::<$preprocessor_type>();
+            for int_byte in 0u8..0xFFu8 {
+                let int_replace_byte = 0xFF - int_byte;
+                let $replace_byte = int_replace_byte.to_be_bytes();
+                let $byte = int_byte.to_be_bytes();
                 $block
             }
         }
@@ -22,47 +24,31 @@ macro_rules! stress_test_preprocessor_fn {
 }
 
 macro_rules! for_each_preprocessor{
-    ($file_writer:ident, $data_len:ident, |$preprocessor:ident, $preprocessor_type:ident| $benchmark_block:block) => {
+    ($file_writer:ident, |$preprocessor:ident, $preprocessor_type_str:ident| $benchmark_block:block) => {
         let mut $preprocessor = $file_writer.preprocess_with::<CharIndexMatrix>();
-        let $preprocessor_type = "CharIndexMatrix";
+        let $preprocessor_type_str = "CharIndexMatrix";
+
         $benchmark_block
         let mut $preprocessor = $file_writer.preprocess_with::<ContinuousHashmap>();
-        let $preprocessor_type = "ContinuousHashmap";
+        let $preprocessor_type_str = "ContinuousHashmap";
+
        $benchmark_block
         let mut $preprocessor = $file_writer.preprocess_with::<WindowsHashmap>();
-        let $preprocessor_type = "WindowsHashmap";
+        let $preprocessor_type_str = "WindowsHashmap";
+
        $benchmark_block
         let mut $preprocessor = $file_writer.preprocess();
-        let $preprocessor_type = "None";
+        let $preprocessor_type_str = "ContinuousHashmap";
+
        $benchmark_block
     };
 }
 
-macro_rules! benchmark_with_group{
-    ($criterion:ident, $benchmark_block: ident, $benchmark_fn: ident, $size:ident) => {
-        let mut benchmark_group = criterion.benchmark_group($benchmark_fn);
-        benchmark_group.throughput(Throughput::Bytes(*$size as u64));
+macro_rules! benchmark_with_group {
+    ($criterion:ident, $benchmark_fn_str: expr, $size:ident, |$benchmark_group:ident| $benchmark_block: block) => {
+        let mut $benchmark_group = $criterion.benchmark_group($benchmark_fn_str);
+        $benchmark_group.throughput(Throughput::Bytes(*$size as u64));
         $benchmark_block
-        benchmark_group.finish();
-    };
-    }
-
-macro_rules! for_each_benchmark_fn {
-    ($criterion:ident, $size:ident, |$benchmark_fn:ident | $benchmark_block:block) => {
-        let $benchmark_fn = "benchmark_find_replace";
-        benchmark_with_group!($criterion, $benchmark_block, $benchmark_fn, $size);
-
-        let $benchmark_fn = "benchmark_find_replace_nth";
-        benchmark_with_group!($criterion, $benchmark_block, $benchmark_fn, $size);
-
-        let $benchmark_fn = "benchmark_rfind_replace";
-        benchmark_with_group!($criterion, $benchmark_block, $benchmark_fn, $size);
-
-        let $benchmark_fn = "benchmark_rfind_replace_nth";
-        benchmark_with_group!($criterion, $benchmark_block, $benchmark_fn, $size);
-
-        let $benchmark_fn = "benchmark_find_replace_all";
-        benchmark_with_group!($criterion, $benchmark_block, $benchmark_fn, $size);
     };
 }
 
@@ -76,108 +62,103 @@ fn gen__dataset(bytes: usize) -> Vec<u8> {
 }
 
 fn benchmark_find_replace_nth<T: Search>(
-    preprocessor: T,
+    preprocessor: &mut T,
     file_writer: &mut FileWriter,
     data_len: usize,
 ) {
     stress_test_preprocessor_fn!(
-        preprocessor,
         T,
         file_writer,
         data_len,
-        |byte, replace_byte, preprocess_cache| {
+        |byte, replace_byte, preprocessor_cache, digit| {
             file_writer.find_replace_nth(
                 black_box(byte),
                 black_box(replace_byte),
                 black_box(digit),
-                &mut preprocess_cache,
+                &mut preprocessor_cache,
             );
         }
     );
 }
 
 fn benchmark_find_replace<T: Search>(
-    preprocessor: T,
+    preprocessor: &mut T,
     file_writer: &mut FileWriter,
     data_len: usize,
 ) {
     stress_test_preprocessor_fn!(
-        preprocessor,
         T,
         file_writer,
         data_len,
-        |byte, replace_byte, preprocess_cache| {
+        |byte, replace_byte, preprocessor_cache, digit| {
             file_writer.find_replace(
                 black_box(byte),
                 black_box(replace_byte),
-                &mut preprocess_cache,
+                &mut preprocessor_cache,
             );
         }
     );
 }
 
 fn benchmark_rfind_replace_nth<T: Search>(
-    preprocessor: T,
+    preprocessor: &mut T,
     file_writer: &mut FileWriter,
     data_len: usize,
 ) {
     stress_test_preprocessor_fn!(
-        preprocessor,
         T,
         file_writer,
         data_len,
-        |byte, replace_byte, preprocess_cache| {
+        |byte, replace_byte, preprocessor_cache, digit| {
             file_writer.rfind_replace_nth(
                 black_box(byte),
                 black_box(replace_byte),
                 black_box(digit),
-                &mut preprocess_cache,
+                &mut preprocessor_cache,
             );
         }
     );
 }
 
 fn benchmark_rfind_replace<T: Search>(
-    preprocessor: T,
+    preprocessor: &mut T,
     file_writer: &mut FileWriter,
     data_len: usize,
 ) {
     stress_test_preprocessor_fn!(
-        preprocessor,
         T,
         file_writer,
         data_len,
-        |byte, replace_byte, preprocess_cache| {
+        |byte, replace_byte, preprocessor_cache, digit| {
             file_writer.rfind_replace(
                 black_box(byte),
                 black_box(replace_byte),
-                &mut preprocess_cache,
+                &mut preprocessor_cache,
             );
         }
     );
 }
 
 fn benchmark_find_replace_all<T: Search>(
-    preprocessor: T,
+    preprocessor: &mut T,
     file_writer: &mut FileWriter,
     data_len: usize,
 ) {
     stress_test_preprocessor_fn!(
-        preprocessor,
         T,
         file_writer,
         data_len,
-        |byte, replace_byte, preprocess_cache| {
+        |byte, replace_byte, preprocessor_cache, digit| {
             file_writer.find_replace_all(
                 black_box(byte),
                 black_box(replace_byte),
-                &mut preprocess_cache,
+                &mut preprocessor_cache,
             );
         }
     );
 }
 
-fn benchmark_preprocessors() {
+fn benchmark_preprocessors(criterion: &mut Criterion) {
     let tempdir = tempdir().unwrap();
     let tempdir_path = tempdir.path();
     let test_file_path = tempdir_path.join("test_data");
@@ -186,25 +167,103 @@ fn benchmark_preprocessors() {
 
     const KB: usize = 1024;
     for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter() {
-        let data = gen__dataset(size);
+        let data = gen__dataset(*size);
         file_writer.overwrite(&data);
-        for_each_benchmark_fn!(criterion, size, |benchmark_fn| {
-            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type| {
-                criterion.bench_with_input(
-                    BenchmarkId::new(preprocessor_type, size),
+        benchmark_with_group!(criterion, "find_replace_nth", size, |benchmark_group| {
+            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type_str| {
+                benchmark_group.bench_with_input(
+                    BenchmarkId::new(preprocessor_type_str, size),
                     &size,
                     |b, &s| {
                         b.iter(|| {
-                            benchmark_fn::<preprocessor_type>(
-                                black_box(preprocessor),
+                            benchmark_find_replace_nth(
+                                black_box(&mut preprocessor),
                                 black_box(&mut file_writer),
-                                black_box(s),
-                            )
+                                black_box(*s),
+                            );
                         });
+                        file_writer.overwrite(&data);
                     },
                 );
-                file_writer.overwrite(&data);
             });
+            benchmark_group.finish();
+        });
+        benchmark_with_group!(criterion, "find_replace", size, |benchmark_group| {
+            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type_str| {
+                benchmark_group.bench_with_input(
+                    BenchmarkId::new(preprocessor_type_str, size),
+                    &size,
+                    |b, &s| {
+                        b.iter(|| {
+                            benchmark_find_replace(
+                                black_box(&mut preprocessor),
+                                black_box(&mut file_writer),
+                                black_box(*s),
+                            );
+                        });
+                        file_writer.overwrite(&data);
+                    },
+                );
+            });
+            benchmark_group.finish();
+        });
+        benchmark_with_group!(criterion, "rfind_replace_nth", size, |benchmark_group| {
+            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type_str| {
+                benchmark_group.bench_with_input(
+                    BenchmarkId::new(preprocessor_type_str, size),
+                    &size,
+                    |b, &s| {
+                        b.iter(|| {
+                            benchmark_rfind_replace_nth(
+                                black_box(&mut preprocessor),
+                                black_box(&mut file_writer),
+                                black_box(*s),
+                            );
+                        });
+                        file_writer.overwrite(&data);
+                    },
+                );
+            });
+            benchmark_group.finish();
+        });
+        benchmark_with_group!(criterion, "rfind_replace", size, |benchmark_group| {
+            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type_str| {
+                benchmark_group.bench_with_input(
+                    BenchmarkId::new(preprocessor_type_str, size),
+                    &size,
+                    |b, &s| {
+                        b.iter(|| {
+                            benchmark_rfind_replace(
+                                black_box(&mut preprocessor),
+                                black_box(&mut file_writer),
+                                black_box(*s),
+                            );
+                        });
+                        file_writer.overwrite(&data);
+                    },
+                );
+            });
+            benchmark_group.finish();
+        });
+
+        benchmark_with_group!(criterion, "find_replace_all", size, |benchmark_group| {
+            for_each_preprocessor!(file_writer, |preprocessor, preprocessor_type_str| {
+                benchmark_group.bench_with_input(
+                    BenchmarkId::new(preprocessor_type_str, size),
+                    &size,
+                    |b, &s| {
+                        b.iter(|| {
+                            benchmark_find_replace_all(
+                                black_box(&mut preprocessor),
+                                black_box(&mut file_writer),
+                                black_box(*s),
+                            );
+                        });
+                        file_writer.overwrite(&data);
+                    },
+                );
+            });
+            benchmark_group.finish();
         });
     }
 }
